12fde74ae82ccb6abf01f1e2fbe198f4a96b8d4b
Monica <monicadolcemascolo@gmail.com>
2023-02-12 14:19:39 -0500

    cleaned up script.js

--- script.js
+++ script.js
@@ -16,40 +16,5 @@ function generateFact() {
         .then((data) => {
         factEl.innerHTML = data.Results[0].text
         })
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
 
 }
-
-
-
-
-
-
-// generateFact()
-
-// function generateFact() {
-//     fetch('https://rest.blackhistoryapi.io/fact/random', {
-//         headers: {
-//             'Accept': 'application/json',
-//             'X-api-key': 'bW9uaWNhZG9sY2VtYXNjb2xvRnJpIE'
-//         }
-//     })
-//     .then((res) => res.json())
-//     .then((data) => {
-//         factEl.innerHTML = data.text
-//     })
-// }
-
-// async function generateFact() {
-//     const config = {
-//         headers: {
-//             Accept: 'application/json'
-//         },
-//     }
-//     const res = await fetch('https://rest.blackhistoryapi.io/fact/random?q={length}&apiKey={bW9uaWNhZG9sY2VtYXNjb2xvRnJpIE}', config)
-    
-//     const data = await res.json()
-
-//     factEl.innerHTML = data.text
-// }
